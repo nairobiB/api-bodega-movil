59d5a066f9fbbc25020cddb242e05f3211908179
Nairobi Bodden <breaniebodden@yahoo.com>
2022-12-06 16:34:21 -0600

    si

--- src/pantallas/Inicio.js
+++ src/pantallas/Inicio.js
@@ -1,25 +1,20 @@
-import { Text, View, Button } from "react-native";
-import React, { Component, useState, useEffect, useContext } from "react";
-import Estilos from "../componentes/Estilos";
-import UsuarioContext from "../contexto/UsuarioContext";
+import { Text, View, Button, Image } from "react-native";
 
+import Estilos from "../componentes/Estilos";
+
+import img from "../../assets/img.png";
 // export default class Inicio extends Component {
 //   render() {
 const Inicio = () => {
-  const { setCerrarSesion } = useContext(UsuarioContext);
-  const cerrarSesion = async () => {
-    await setCerrarSesion();
-  };
   return (
     <View style={{ justifyContent: "center", flex: 1 }}>
-      <Text style={{ alignSelf: "center" }}>
-        Bienvenido a la pagina de inicio
+      <Image source={img} style={{ alignSelf: "center" }}></Image>
+      <Text style={{ fontSize: 20, alignSelf: "center" }}>
+        Bienvenido a la app para administrar
       </Text>
-      <Button
-        style={Estilos.btnLogin}
-        title="Cerrar sesión"
-        onPress={cerrarSesion}
-      ></Button>
+      <Text style={{ fontSize: 20, alignSelf: "center" }}>
+        Bodega "Todo Cabe"
+      </Text>
     </View>
   );
   //}
--- src/pantallas/Perfil.js
+++ src/pantallas/Perfil.js
@@ -1,12 +1,25 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
-
-export default class Perfil extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Perfil</Text>
-      </View>
-    );
-  }
-}
+import { Text, View, Button, Image } from "react-native";
+import React, { Component, useState, useEffect, useContext } from "react";
+import Estilos from "../componentes/Estilos";
+import UsuarioContext from "../contexto/UsuarioContext";
+import img from "../../assets/img.png";
+// export default class Inicio extends Component {
+//   render() {
+const Perfil = () => {
+  const { setCerrarSesion } = useContext(UsuarioContext);
+  const cerrarSesion = async () => {
+    await setCerrarSesion();
+  };
+  return (
+    <View style={{ justifyContent: "center", flex: 1 }}>
+      <Text style={{ fontSize: 20, alignSelf: "center" }}>Cerrar sesión</Text>
+      <Button
+        style={Estilos.btnLogin}
+        title="Cerrar sesión"
+        onPress={cerrarSesion}
+      ></Button>
+    </View>
+  );
+  //}
+};
+export default Perfil;
