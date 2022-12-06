888dece40e0fde94b9171532bd788ec7af30b5e0
Nairobi Bodden <breaniebodden@yahoo.com>
2022-12-05 22:33:25 -0600

    no

--- src/componentes/Roles.js
+++ src/componentes/Roles.js
@@ -1,35 +1,61 @@
 import { View, Image, StyleSheet, Text, Checkbox } from "react-native";
-//import Checkbox from 'expo-checkbox';
-import { Icon, Group, Button, Divider } from "native-base";
+import {
+  Icon,
+  Group,
+  Button,
+  Divider,
+  Modal,
+  FormControl,
+  Input,
+} from "native-base";
+import Axios from "./Axios";
+import UsuarioContext from "../contexto/UsuarioContext";
 import React, { useState, useEffect, useContext } from "react";
 import login from "../../assets/login.jpg";
+import { useNavigation } from "@react-navigation/native";
 import { Feather, MaterialIcons } from "@expo/vector-icons";
 import Estilos from "./Estilos";
-import { useNavigation } from "@react-navigation/native";
-// const uriImagen = Image.resolveAssetSource(login).uri;
-//import { urlImagenesRoles } from '../configuracion/Urls';
-const Roles = (props) => {
-  const nav = useNavigation();
-  const [Check, setCheck] = useState(false);
-  // const [imagen, setImagen] = useState(uriImagen);
-  // useEffect(() =>{
-  //     setImagen(cargarImagen);
-  //     // setCheck(props.rol.activo);
-  // }, []);
 
-  // const cargarImagen = () =>{
-  //     console.log(props.rol.imagen);
-  //     if(props.rol.imagen == null){
-  //         setImagen(uriImagen);
-  //     }
-  //     else{
-  //         setImagen(urlImagenesRoles + props.rol.imagen);
-  //     }
-  //     return imagen;
-  // }
+const Usuarios = (props) => {
+  const { token } = useContext(UsuarioContext);
+
+  const nav = useNavigation();
+  const [showModal, setShowModal] = useState(false);
+  const presionar = () => {
+    const idRol = props.rol.id;
+    const rolname = props.rol.nombreRol;
+    nav.navigate("editar", { id: idRol, antiguoRol: rolname });
+    console.log(idRol);
+  };
+  const eliminar = async (data) => {
+    var textoMensaje = "";
+    try {
+      Axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      await Axios.delete("/roles/eliminar?id=" + props.rol.id)
+
+        .then(async (data) => {
+          console.log(data);
+          const json = data.data;
+          if (json.errores.length == 0) {
+          } else {
+            json.errores.forEach((element) => {
+              textoMensaje += element.mensaje + ". ";
+            });
+          }
+          console.log(data);
+        })
+        .catch((error) => {
+          textoMensaje = error;
+        });
+    } catch (error) {
+      textoMensaje = error;
+      console.log(error);
+    }
+    setShowModal(false);
+  };
+
   return (
     <View style={Estilos.contenedorTipo}>
-      <Image style={Estilos.imagen} source={login} />
       <View style={Estilos.contenedorTexto}>
         <Text>ID: {props.rol.id}</Text>
         <Text>{props.rol.nombreRol}</Text>
@@ -48,11 +74,7 @@ const Roles = (props) => {
           <Button
             startIcon={<Icon as={Feather} name="edit" size={4}></Icon>}
             colorScheme="darkBlue"
-            onPress={() =>
-              nav.navigate("editar", {
-                id: props.rol.id,
-              })
-            }
+            onPress={presionar}
           >
             Editar
           </Button>
@@ -60,13 +82,42 @@ const Roles = (props) => {
             startIcon={<Icon as={MaterialIcons} name="delete" size={4}></Icon>}
             variant="solid"
             colorScheme="red"
+            onPress={() => setShowModal(true)}
           >
             Eliminar
           </Button>
         </View>
       </View>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Content maxWidth="400px">
+          <Modal.CloseButton />
+          <Modal.Header>Eliminar registro</Modal.Header>
+          <Modal.Body>
+            <FormControl>
+              <FormControl.Label>Nombre del rol</FormControl.Label>
+              <Input value={props.rol.nombreRol} editable={false} />
+            </FormControl>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setShowModal(false);
+                }}
+              >
+                Cancelar
+              </Button>
+              <Button variant="solid" colorScheme="red" onPress={eliminar}>
+                Eliminar
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
     </View>
   );
 };
 
-export default Roles;
+export default Usuarios;
