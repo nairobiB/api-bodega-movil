d8b0b783403c7a737cc1bd83acde9cf5490b1a6b
Nairobi Bodden <breaniebodden@yahoo.com>
2022-12-06 18:13:33 -0600

    11223sc

--- src/componentes/AxiosImagen.js
+++ src/componentes/AxiosImagen.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const AxiosImagen = axios.create({
-  baseURL: "http://192.168.1.78:4001/api/",
+  baseURL: "http://192.168.0.13:4001/api/",
   timeout: 5000,
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'multipart/form-data' }
+    Accept: "application/json",
+    "Content-Type": "multipart/form-data",
+  },
 });
 export default AxiosImagen;
